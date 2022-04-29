2649d19692377a6d73bec101c5f7cf2db6a37b35
Snubb <oliver.lundqvist@elev.ga.ntig.se>
2022-04-29 10:07:23 +0200

    Fixat en post form, använder ingen user just nu så author och uploader är hårdkodat.

--- routes/videos.js
+++ routes/videos.js
@@ -40,4 +40,36 @@ router.get('/post', async (req, res, next) => {
     res.render('postVideo.njk', data)
 });
 
+router.post('/post',
+    async (req, res, next) => {
+        const videoURL = req.body.videourl;
+        const videoID = videoURL.split('v=')[1];
+
+
+        const sql = 'INSERT INTO videos (videourl, videoID, author, uploader) VALUES (?, ?, ?, ?)';
+        await pool.promise()
+        .query(sql, [videoURL, videoID, 'Oliver', 'Oliver'])
+        .then((response) => {
+            console.log(response);
+            if (response[0].affectedRows == 1) {
+                res.redirect('/videos');
+            } else {
+                res.status(400).json({
+                    videos: {
+                        error: "Invalid video"
+                    }
+                })
+            }
+            
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                videos: {
+                    error: "Cannot retrieve videos"
+                }
+            });
+        });
+});
+
 module.exports = router;
