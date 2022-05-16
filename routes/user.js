a2042ffc584c3ba029fe2ea64638003303a43fd3
Snubb <oliver.lundqvist@elev.ga.ntig.se>
2022-05-16 11:11:32 +0200

    Fixat någon form av /user/:user

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,33 @@ router.get('/', async (req, res, next) => {
   await pool.promise()
   .query('SELECT * FROM videos JOIN users ON users.name = videos.uploader WHERE videos.uploader = ?', [req.session.loginToken])
   .then(([rows, fields]) => {
-    res.render("user.njk", {data: rows, username: req.session.loginToken})
+    newRows = rows.splice(0, 3);
+    res.render("user.njk", {data: newRows, username: req.session.loginToken})
+  })
+});
+
+router.get('/:user', async (req, res, next) => {
+  username = req.params.user;
+  await pool.promise()
+  .query('SELECT * FROM users WHERE name = ?', [username])
+  .then(([rows]) => {
+    console.log(rows)
+    console.log(rows.length)
+    if (rows.length == 0) {
+      console.log("empty")
+      res.json({
+        tasks: {
+            error: "ID does not exist"
+        }
+      });
+      return;
+    }  
+  });
+  await pool.promise()
+  .query('SELECT * FROM videos JOIN users ON users.name = videos.uploader WHERE videos.uploader = ?', [username])
+  .then(([rows, fields]) => {
+    newRows = rows.splice(0, 3);
+    res.render("user.njk", {data: newRows, username: username})
   })
 });
 module.exports = router;
